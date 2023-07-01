25504fd4ee8fe647c5ad2745d782b34975c0e61f
Joseph Karanja <joseph.karanja@student.moringaschool.com>
2023-07-02 02:18:22 +0300

    complete assignemnt

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+document.querySelector("#user").addEventListener("submit", takesubmit)
+
+function takesubmit(e){
+    e.preventDefault()
+    let user1 = {
+        name:e.target.name.value,
+        Email:e.target.Email.value
+    }
+    renderuser(user1)
+    submitData(user1)
+}
+
+function renderuser(user1){
+    let userprofile = document.createElement("p")
+    userprofile.className = "users"
+    userprofile.innerHTML = `
+    <p>${user1.name}</p>
+    <p>${user1.Email}</p>
+    `
+    document.querySelector("#uselist").appendChild(userprofile)
+}
+
+function submitData(user1){
+    fetch("http://localhost:3000/users", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user1)
+    })
+    .then(res => res.json())
+    .then(user => console.log(user1))
+}
